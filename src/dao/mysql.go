d98b17caee7cb49a8e30b157eeb240bce2deb779
LIUCJ <593562829@qq.com>
2022-06-14 10:10:48 +0800

    设置数据库连接池

--- src/dao/mysql.go
+++ src/dao/mysql.go
@@ -56,6 +56,10 @@ func InitMySql() (err error) {
 	if err != nil {
 		panic(err)
 	}
+	//设置数据库连接池
+	sqlDB := SqlSession.DB()
+	sqlDB.SetMaxOpenConns(100) //设置数据库连接池最大连接数
+	sqlDB.SetMaxIdleConns(20)  //连接池最大允许的空闲连接数，如果没有sql任务需要执行的连接数大于20，超过的连接会被连接池关闭。
 	// 验证数据库连接是否成功，若成功，则无异常
 	return SqlSession.DB().Ping()
 }
